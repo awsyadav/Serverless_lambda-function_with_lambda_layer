44c093537bcfb1ec16d2e059524374304560c4aa
awsyadav <45893649+awsyadav@users.noreply.github.com>
2019-07-09 06:52:43 +0530

    Delete provider.tf

--- serverless_lambda_s3invoke/provider.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "eu-west-1"
- /* assume_role {
-    role_arn = "arn:aws:iam::441275923298:role/bacl-ibm-allowterraform"
-  }*/
-  access_key  = "AKIAWNPQYHNRHBPMKQGX"
-  secret_key  = "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"
-}
-
